f2b6a86fd2fa77f0c2cd0292cb63f44723f5ff2a
carlsbad505 <carl@tabrific.com>
2015-11-13 16:32:37 +0000

    modify name validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
   
-  validates :name, presence: true, length: { minimum: 1 }       
+  validates :name, on: :create, on: :update, presence: true, length: { minimum: 1 }       
   validates :email,
             presence: true,
             uniqueness: { case_sensitive: false },
